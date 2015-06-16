99c7d4a977a457dd8c4be12c3c6e0a8a97e1d53b
anzeo <zebigbozz@gmail.com>
2015-06-16 13:43:41 +0200

    Added pathing info
    Inside angular2 src packages are sometimes referenced to as
    'angular2/src/...'. Added a paths entry to resolve these as
    'node_modules/angular2/...'

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 System.config({
-    baseURL: '/',
     transpiler: "typescript",
     packages: {
         '/src': {
             main: 'app',
             defaultExtension: 'ts'
         },
-        'node_modules/angular2': {
+        '/node_modules/angular2': {
             main: 'angular2',
-            defaultExtension: 'js',
-            map: {
-
-            }
+            defaultExtension: 'js'
         }
+    },
+    paths: {
+        "angular2/*": "/node_modules/angular2/*"
     },
     map: {
         typescript: "/lib/typescript@v1.5-beta2/bin/typescript.js"
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 "use strict";
 
 import Dependency from 'src/dependency';
-import {View, Component, bootstrap} from '/node_modules/angular2/angular2';
+import {View, Component, bootstrap} from 'angular2/angular2';
 
 let dep = new Dependency();
 dep.say();
 
-//
-//@Component({
-//    selector: 'my-app'
-//})
-//@View({
-//    template: '<h1>Hello {{ name }}</h1>'
-//})
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+})
 class MyAppComponent {
     name: string;
 
